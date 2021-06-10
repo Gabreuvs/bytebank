b5c66b3755775cc1f8b2448d6f15e4bb882e45a3
Gabriel Silva <gabreuvs@hotmail.com>
2021-06-09 21:25:31 -0300

    new event in component

--- src/app/Nova-transferencia/nova-transferencia.component.ts
+++ src/app/Nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,12 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number | undefined;
   destino: number | undefined;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+
+  }
+
+  limparCampos() {
+    this.valor = undefined;
+    this.destino = undefined;
   }
 }
